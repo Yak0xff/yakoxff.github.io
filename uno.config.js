ec41236bc88a0762656413c38044ffa256bd93cb
Eltrac <hi@guhub.cn>
2025-07-17 11:25:10 +0800

    refactor: avoid using straight value in UnoCSS

--- uno.config.js
+++ uno.config.js
@@ -1,48 +1,14 @@
 import { defineConfig, presetWind3, presetAttributify } from 'unocss'
 
+const range = (n, fn) => Array.from({ length: n }, (_, i) => fn(i));
+
+function rangeWithPrefixes(n, base, prefixes = ['']) {
+  return prefixes.flatMap(prefix =>
+    range(n, i => `${prefix}${base}-${i}`)
+  );
+}
+
 export default defineConfig({
-  safelist: [
-      'text-sm',
-      'text-md',
-      'text-lg',
-      'text-xl',
-      'text-2xl',
-      'text-3xl',
-      'text-4xl',
-      'text-5xl',
-
-      // rotate -4 to +4 deg
-      'rotate-[-3.0deg]',
-      'rotate-[-2.0deg]',
-      'rotate-[-1.0deg]',
-      'rotate-[0.0deg]',
-      'rotate-[1.0deg]',
-      'rotate-[2.0deg]',
-      'rotate-[3.0deg]',
-
-      // opacity from 0.6 to 1.0
-      'opacity-[0.6]',
-      'opacity-[0.7]',
-      'opacity-[0.8]',
-      'opacity-[0.9]',
-      'opacity-[1.0]',
-
-      'absolute',
-      'right-0',
-      'w-[5rem]',
-      'h-[5rem]',
-      'top-[2rem]',
-      'w-[calc(25%_-_1rem)]',
-      'w-[calc(50%_-_2rem)]',
-      'w-[10rem]',
-      'sm:w-full',
-      'right-[-3rem]',
-      'top-[1rem]',
-      'md:w-[7rem]',
-      'md:h-[7rem]',
-      'md:right-[-5rem]',
-      'top-[-1rem]', 'right-[-1rem]', 'w-[4rem]', 'h-[4rem]', 'z-1', 'rotate-[-5deg]'
-    ],
   content: {
     filesystem: ['./**/*.{html,md,js}'],
   },
@@ -62,5 +28,22 @@ export default defineConfig({
   shortcuts: {
     'small-button': 'bg-blockBg px-2 py-1 no-underline hover:no-underline rounded border border-solid border-bg hover:border-textColor transition',
     'round-sticker': 'bg-blockBg rounded-full shadow-sm p-4'
-  }
+  },
+  safelist: [
+    'text-sm', 'text-md', 'text-lg', 'text-xl', 'text-2xl', 'text-3xl', 'text-4xl', 'text-5xl',
+
+    ...range(10, i => `opacity-${i * 10}`),
+    ...rangeWithPrefixes(5, 'rotate', ['', '-']),
+    ...rangeWithPrefixes(30, 'w', ['', 'md:', 'lg:']),
+    ...rangeWithPrefixes(30, 'h', ['', 'md:']),
+    ...rangeWithPrefixes(30, 'right', ['', 'md:', '-', 'md:-']),
+    ...rangeWithPrefixes(30, 'top', ['', '-']),
+
+    // special cases
+    'absolute',
+    'w-[calc(25%_-_1rem)]',
+    'w-[calc(50%_-_2rem)]',
+    'w-[10rem]',
+    'sm:w-full',
+  ]
 })
